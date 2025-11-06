2d37103fa2efa21a34a781320b1e670964b2f0de
Victor Manuel Silva Ormeno <victorsilvaormeno@gmail.com>
2025-11-06 19:26:10 -0300

    creacion database.py

---
+++ app/core/database.py
@@ -0,0 +1,21 @@
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:Vicente5150.@localhost:3306/userdb")
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,  # muestra las consultas SQL en consola
+    pool_pre_ping=True  # reconecta si hay desconexión
+)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Dependencia para FastAPI
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
