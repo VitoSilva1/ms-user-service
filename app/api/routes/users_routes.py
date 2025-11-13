2ae1c7cb7025c69e48b83c54d150c8433d809281
Victor Manuel Silva Ormeno <victorsilvaormeno@gmail.com>
2025-11-13 19:43:17 -0300

    creacion ms-user-service

--- app/api/routes/users_routes.py
+++ app/api/routes/users_routes.py
@@ -1,18 +1,23 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from .. import schemas, crud
-from ..database import get_db
+
+from app.core.database import get_db
+from app.schemas import user_schema
+from app.services import user_service
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/", response_model=schemas.UserResponse)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    return crud.create_user(db, user)
 
-@router.get("/", response_model=list[schemas.UserResponse])
+@router.post("/", response_model=user_schema.UserResponse)
+def create_user(user: user_schema.UserCreate, db: Session = Depends(get_db)):
+    return user_service.create_user(db, user)
+
+
+@router.get("/", response_model=list[user_schema.UserResponse])
 def list_users(db: Session = Depends(get_db)):
-    return crud.get_users(db)
+    return user_service.get_all_users(db)
 
-@router.get("/{user_id}", response_model=schemas.UserResponse)
+
+@router.get("/{user_id}", response_model=user_schema.UserResponse)
 def get_user(user_id: int, db: Session = Depends(get_db)):
-    return crud.get_user_by_id(db, user_id)
+    return user_service.get_user_by_id(db, user_id)
